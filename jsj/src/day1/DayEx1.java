80af474c1c0f33d038662d71bb6db521996f9397
sjjang <sjjang0322@naver.com>
2021-10-27 17:30:38 +0900

    Create DayEx1.java

---
+++ jsj/src/day1/DayEx1.java
@@ -0,0 +1,32 @@
+package day1;
+
+public class DayEx1 {
+
+	public static void main(String[] args) {
+		/*핸드폰 번호를 -없이 저장하여 관리하려 한다. 알맞은 변수를 선언하고
+		 * 초기값으로 01012345678을 저장하여 확인해보세요.
+		 */
+		
+		String phnum1="01012345678";
+		
+		System.out.println(phnum1);
+		
+		
+		/* 성별을 저장하여 관리하려 한다. 알맞은 변수를 선언하고 초기값으로 남자를 설정해보세요.
+		 * 단, 성별은 남자와 여자만 있다고 가정.
+		 */
+		
+		String gender1 = "남자";
+		char gender2 = '남';
+		
+		System.out.println(gender1);
+		System.out.println(gender2 + "자");
+		
+		boolean isMale = true;
+		int isMale2 = 13;//13:남자, 24:여자라는걸 기억해야하는 불편함이 있다
+		
+		
+		
+	}
+
+}
