8141e4f00a2f5c72c380e0f4594e10f9fb9cc41b
sjjang0322 <sjjang0322@naver.com>
2021-10-29 17:25:29 +0900

    Update ForHelloEx1.java

--- jsj/src/day3/ForHelloEx1.java
+++ jsj/src/day3/ForHelloEx1.java
@@ -3,8 +3,8 @@ package day3;
 public class ForHelloEx1 {
 
 	public static void main(String[] args) {
-		/*Hello를 10번 출력하는 코드를 작성하세요*/
-		for(int i=0;i<10;i++) {
+		/*Hello를 5번 출력하는 코드를 작성하세요*/
+		for(int i=0;i<5;i++) {
 			System.out.println("Hello");
 		}
 	}
