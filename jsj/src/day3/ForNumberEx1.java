cd43a14dcdebb222769193e2a69bebcc90e540ab
sjjang0322 <sjjang0322@naver.com>
2021-10-29 17:25:35 +0900

    Create ForNumberEx1.java

---
+++ jsj/src/day3/ForNumberEx1.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class ForNumberEx1 {
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력하는 코드를 for문으로 작성하세요.
+		 * 
+		 */
+		for(int i=0;i<5;) {
+			System.out.println(++i);
+		}
+	}
+}
