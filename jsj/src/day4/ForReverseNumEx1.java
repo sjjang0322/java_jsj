ac8953d8b735bb4c57ed184a84342abd06c7a35e
sjjang0322 <sjjang0322@naver.com>
2021-11-01 11:12:47 +0900

    Create ForReverseNumEx1.java

---
+++ jsj/src/day4/ForReverseNumEx1.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class ForReverseNumEx1 {
+
+	public static void main(String[] args) {
+		/* 5부터 1까지 출력하는 예제를 for문을 이용하여 작성하세요.
+		 * 
+		 */
+		for(int i = 0;i<5;i++) {
+			System.out.println((5-i));
+		}
+	}
+
+}
