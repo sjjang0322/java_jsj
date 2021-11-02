a183d3a18f58b3f6568762f97be6df596a16ecb6
boram1219 <guenlove9293@naver.com>
2021-11-02 16:22:09 +0900

    Update StarEx4.java

--- jsj/src/day5/StarEx4.java
+++ jsj/src/day5/StarEx4.java
@@ -21,7 +21,65 @@ public class StarEx4 {
 			System.out.println();
 		}
 		
+		/* *****
+		 * ****
+		 * ***
+		 * **
+		 * *
+		 * *****
+		 *  ****
+		 *   ***
+		 *    **
+		 *     *
+		 *   *
+		 *  ***
+		 * *****
+		 *  ***
+		 *   *
+		 */
 		
+		System.out.println("-----");
+
+		for(int i=5;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("-----");
+		
+		for(int i=5;i>=1;i--) {
+			for(int j=5;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("-----");
+		
+		for(int i=1;i<=2;i++) {
+			for(int j=3;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i=3;i>=1;i--) {
+			for(int j=3;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
 	}
 
