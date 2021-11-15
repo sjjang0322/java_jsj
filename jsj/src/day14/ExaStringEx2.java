41cb500d53d0ffa588dbf97754ef931f7ed066f8
sjjang0322 <sjjang0322@naver.com>
2021-11-15 11:28:44 +0900

    Create ExaStringEx2.java

---
+++ jsj/src/day14/ExaStringEx2.java
@@ -0,0 +1,26 @@
+package day14;
+
+public class ExaStringEx2 {
+
+	public static void main(String[] args) {
+		/* 5명의 이름을 저장하고, 저장된 이름중 홍길동이 있는지 확인하세요.
+		 */
+		
+		String name[] = new String[] {"홍길동", "안중근", "홍길동이", "김홍길동", "홍진호"};
+		String search[] = new String[5];
+		int count=0;
+		for(String tmp : name) {
+			if(tmp == "홍길동") {
+				count++;
+			}
+		}
+		if(count==0) {
+			System.out.println("홍길동이 없습니다.");
+		} else {
+			System.out.println("홍길동이 " + count + "명 있습니다.");
+		}
+		count=0;
+	
+	}
+
+}
