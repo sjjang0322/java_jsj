dba90c1c3b7a6d84e74790a568015d2f2bd8193d
jsj <guenlove9293@naver.com>
2021-12-23 17:49:40 +0900

    dd

---
+++ 프론트/js/rolling.js
@@ -0,0 +1,21 @@
+function rollingToTop(parentSelector, childSelector, moveTime, animationTime){
+	var height = $(parentSelector).height();
+	return setInterval(function(){					
+		if(!$(parentSelector+'>'+childSelector).first().is(':animated')){
+			$(parentSelector+'>'+childSelector).first().animate({marginTop:-	height},animationTime,function(){
+				$(this).removeAttr('style').detach().appendTo(parentSelector);
+			});
+		}					
+	}, moveTime);
+}
+
+function rollingToLeft(parentSelector, childSelector, moveTime, animationTime){
+	var width = $(parentSelector).width();
+	return setInterval(function(){					
+		if(!$(parentSelector+'>'+childSelector).first().is(':animated')){
+			$(parentSelector+'>'+childSelector).first().animate({marginLeft:-	width},animationTime,function(){
+				$(this).removeAttr('style').detach().appendTo(parentSelector);
+			});
+		}					
+	}, moveTime);
+}
