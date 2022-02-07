0a46ad7b0da89d7f2842b5faecc1b3d16891376c
sjjang0322 <sjjang0322@naver.com>
2022-02-07 11:35:15 +0900

    dd

--- spring_test/src/main/java/kr/green/test/service/MemberServiceImp.java
+++ spring_test/src/main/java/kr/green/test/service/MemberServiceImp.java
@@ -109,6 +109,7 @@ public class MemberServiceImp implements MemberService {
 	        mailSender.send(message);
 	    } catch(Exception e){
 	        System.out.println(e);
+	        return "error";
 	    }
 
 	    return "true";
